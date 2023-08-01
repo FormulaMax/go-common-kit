72528bcceb615670c35b8b6c4d6fff553a4b5974
Formula <mmmmmm_2345@163.com>
2023-08-02 01:01:47 +0800

    Add base folders

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	Slice "homework/go-common/slice"
+)
+
+func main() {
+	fmt.Println("Hello")
+	Slice.Delete()
+}
---
+++ slice/delete.go
@@ -0,0 +1,5 @@
+package Slice
+
+func Delete() {
+	print("Slice")
+}
